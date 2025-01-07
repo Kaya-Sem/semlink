1c86e4ff2e0469be7732246e70b5f92d9c278477
Kaya-Sem <kayasemvc@gmail.com>
2025-01-07 20:41:59 +0100

    updated xattr functions

--- cmd/xattr.go
+++ cmd/xattr.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"strings"
 
 	"golang.org/x/sys/unix"
 )
@@ -13,4 +14,45 @@ func setXattr(path string, semlinkXattrKey string, value string) {
 	if err != nil {
 		log.Fatalf("Failed to set xattr: %v", err)
 	}
+}
+
+func getXattr(path string, semlinkXattrKey string) string {
+	value := make([]byte, 1024)
+	vLen, err := unix.Getxattr(path, semlinkXattrKey, value)
+
+	if err != nil {
+		if err == unix.ENODATA {
+			log.Fatalf("Nothing found for %s with key %s\n", path, semlinkXattrKey)
+		}
+
+		log.Fatalf("Failed to get xattr value: %v", err)
+	}
+
+	rawValue := string(value[:vLen])
+	return rawValue
+}
+
+func getSemlinkType(path string) string {
+	return getXattr(path, semlinkTypeXattrKey)
+}
+
+func getSemlinkTags(path string) []string {
+	tagString := getXattr(path, semlinkTagXattrKey)
+
+	tags := parseTags(tagString)
+
+	return tags
+}
+
+func parseTags(tagString string) []string {
+	if tagString == "" {
+		return []string{}
+	}
+
+	// Split the string by comma and trim spaces
+	tags := strings.Split(tagString, ",")
+	for i, tag := range tags {
+		tags[i] = strings.TrimSpace(tag)
+	}
+	return tags
 }
